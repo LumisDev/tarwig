200754ffea3a1fa68c24d81c76bc073b18f20b22
LumisDev <153009693+LumisDev@users.noreply.github.com>
2023-12-06 20:43:24 +0100

    New updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
-import { Html, MeshDistortMaterial, OrbitControls } from "@react-three/drei";
-import { motion as fm } from "framer-motion";
+import { Canvas } from "@react-three/fiber";
 import React from "react";
-
+import Experience from "./Experience";
 const App = () => {
      return (
-          <>
-               <Html>
-                    <fm.div
-                         className="flex flex-col items-center justify-center"
-                         initial={{ opacity: 0, y: 100 }}
-                         transition={{ duration: 1 }}
-                         animate={{ opacity: 1, y: 0 }}
-                    >
-                         <h1 className="text-green-500 text-3xl font-extrabold underline">
-                              Hello!
-                         </h1>
-                         <div>
-                              Here's a little tarwig for
-                              <span className="text-yellow-400 transition-all hover:text-green-600 font-bold">
-                                   {" "}
-                                   FUN!!!
-                              </span>
-                         </div>
-                    </fm.div>
-               </Html>
-               <pointLight position={[1, 1, 1]} />
-               <ambientLight intensity={5} />
-               <OrbitControls />
-               <mesh>
-                    <sphereGeometry radius={1} />
-                    <MeshDistortMaterial
-                         distort={1}
-                         speed={5}
-                         color="#ffaa33"
-                    />
-               </mesh>
-          </>
+          <Canvas>
+               <Experience />
+          </Canvas>
      );
 };
 
---
+++ src/Experience.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Html, MeshDistortMaterial, OrbitControls } from "@react-three/drei";
+import { motion as fm } from "framer-motion";
+
+const Experience = () => {
+     return (
+          <>
+               <Html>
+                    <fm.div
+                         className="flex flex-col items-center justify-center"
+                         initial={{ opacity: 0, y: 100 }}
+                         transition={{ duration: 1.5, delay: 0.5 }}
+                         animate={{ opacity: 1, y: 0 }}
+                    >
+                         <h1 className="text-green-500 text-3xl font-extrabold underline">
+                              Hello!
+                         </h1>
+                         <div>
+                              Here's a little tarwig for
+                              <span className="text-yellow-400 transition-all hover:text-green-600 font-bold">
+                                   {" "}
+                                   FUN!!!
+                              </span>
+                         </div>
+                    </fm.div>
+               </Html>
+               <pointLight position={[1, 1, 1]} />
+               <ambientLight intensity={5} />
+               <OrbitControls />
+               <mesh>
+                    <sphereGeometry radius={1} />
+                    <MeshDistortMaterial
+                         distort={1}
+                         speed={5}
+                         color="#ffaa33"
+                    />
+               </mesh>
+          </>
+     );
+};
+
+export default Experience;
