858f72ad2f4393f0856f2fa97484fb5c2c3064b8
LumisDev <153009693+LumisDev@users.noreply.github.com>
2023-12-06 17:05:18 +0100

    Updated (again)!

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
      theme: {
           extend: {
                fontFamily: {
-                    sans: ["Poppins", "sans-serif"],
+                    sans: ["Poppins", "Segoe UI", "sans-serif"],
                },
           },
      },
