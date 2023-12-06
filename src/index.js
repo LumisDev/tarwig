7855aaea3aac9ecc1f7080d53089d76f5dcbc6a0
LumisDev <153009693+LumisDev@users.noreply.github.com>
2023-12-06 16:50:00 +0100

    Updated the site

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./main.css";
+import { Canvas } from "@react-three/fiber";
+import App from "./App";
+
+createRoot(document.getElementById("root")).render(<div className="bg-gradient-to-t from-pink-800 to-slate-500 w-screen h-screen"><Canvas><App /></Canvas></div>);
