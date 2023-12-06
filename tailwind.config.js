afe0836c25b3bff7034263baf79d350fb8ffb1b3
LumisDev <153009693+LumisDev@users.noreply.github.com>
2023-12-06 17:03:32 +0100

    Updated (again the website)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    'src/**/*.{js,jsx}'
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+     content: ["src/**/*.{js,jsx}"],
+     theme: {
+          extend: {
+               fontFamily: {
+                    sans: ["Poppins", "sans-serif"],
+               },
+          },
+     },
+     plugins: [],
+};
